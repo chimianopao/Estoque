63db63e85f52dc5a3843667fa0b97d7425408b75
Theilor Raddatz <theilor@gmail.com>
2021-09-12 00:10:11 -0300

    movimentação retorna os produtos

--- Estoque/Estoque/FrmMovimentacao.Designer.cs
+++ Estoque/Estoque/FrmMovimentacao.Designer.cs
@@ -29,6 +29,15 @@
             this.label1 = new System.Windows.Forms.Label();
             this.comboBoxVendedores = new System.Windows.Forms.ComboBox();
             this.buttonCadastraVendedor = new System.Windows.Forms.Button();
+            this.dataGridMovimentacao = new System.Windows.Forms.DataGridView();
+            this.Codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Descricao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Fabricante = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Quantidade = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Valor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridMovimentacao)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -57,16 +66,83 @@
             this.buttonCadastraVendedor.Text = "Cadastrar Vendedora";
             this.buttonCadastraVendedor.UseVisualStyleBackColor = true;
             // 
+            // dataGridMovimentacao
+            // 
+            this.dataGridMovimentacao.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridMovimentacao.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Codigo,
+            this.Descricao,
+            this.Fabricante,
+            this.Quantidade,
+            this.Valor});
+            this.dataGridMovimentacao.Location = new System.Drawing.Point(15, 69);
+            this.dataGridMovimentacao.Name = "dataGridMovimentacao";
+            this.dataGridMovimentacao.Size = new System.Drawing.Size(726, 288);
+            this.dataGridMovimentacao.TabIndex = 3;
+            this.dataGridMovimentacao.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridMovimentacao_CellContentClick);
+            this.dataGridMovimentacao.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridMovimentacao_CellValueChanged);
+            this.dataGridMovimentacao.ColumnHeaderCellChanged += new System.Windows.Forms.DataGridViewColumnEventHandler(this.DataGridView1_ColumnHeaderCellChanged);
+            //this.dataGridMovimentacao.EditingControl.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridMovimentacao_KeyDown);
+            // 
+            // Codigo
+            // 
+            this.Codigo.HeaderText = "Código";
+            this.Codigo.Name = "Codigo";
+            // 
+            // Descricao
+            // 
+            this.Descricao.HeaderText = "Descrição";
+            this.Descricao.Name = "Descricao";
+            this.Descricao.ReadOnly = true;
+            // 
+            // Fabricante
+            // 
+            this.Fabricante.HeaderText = "Fabricante";
+            this.Fabricante.Name = "Fabricante";
+            this.Fabricante.ReadOnly = true;
+            // 
+            // Quantidade
+            // 
+            this.Quantidade.HeaderText = "Quantidade";
+            this.Quantidade.Name = "Quantidade";
+            // 
+            // Valor
+            // 
+            this.Valor.HeaderText = "Valor";
+            this.Valor.Name = "Valor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(545, 388);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Valor Total: R$";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(545, 364);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Quantidade Total:";
+            // 
             // FrmMovimentacao
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridMovimentacao);
             this.Controls.Add(this.buttonCadastraVendedor);
             this.Controls.Add(this.comboBoxVendedores);
             this.Controls.Add(this.label1);
             this.Name = "FrmMovimentacao";
             this.Text = "Movimentação";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridMovimentacao)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -77,5 +153,13 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox comboBoxVendedores;
         private System.Windows.Forms.Button buttonCadastraVendedor;
+        private System.Windows.Forms.DataGridView dataGridMovimentacao;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Codigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Descricao;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Fabricante;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Quantidade;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Valor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
--- Estoque/Estoque/FrmMovimentacao.cs
+++ Estoque/Estoque/FrmMovimentacao.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.Sqlite;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,6 +11,7 @@ using System.Windows.Forms;
 
 namespace Estoque {
     public partial class FrmMovimentacao : Form {
+        string pathSQL = System.IO.Path.Combine(Environment.CurrentDirectory, @"sql\", "estoque.db");
         public FrmMovimentacao()
         {
             InitializeComponent();
@@ -19,5 +21,125 @@ namespace Estoque {
         {
 
         }
+
+        private void dataGridMovimentacao_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridMovimentacao_KeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("olaaa");
+            e.SuppressKeyPress = true;
+            int iColumn = dataGridMovimentacao.CurrentCell.ColumnIndex;
+            int iRow = dataGridMovimentacao.CurrentCell.RowIndex;
+            //MessageBox.Show()
+            //if (e.KeyData == Keys.Enter || e.KeyData == Keys.Down)
+            if (e.KeyData == Keys.Enter && iColumn == 0)
+            {
+                MessageBox.Show("coluna zero");
+                dataGridMovimentacao.CurrentCell = dataGridMovimentacao[3, iRow];
+            }
+            else
+                dataGridMovimentacao.CurrentCell = dataGridMovimentacao[iColumn + 1, iRow];
+        }
+
+        private void DataGridView1_ColumnHeaderCellChanged(Object sender, DataGridViewColumnEventArgs e)
+        {
+
+            System.Text.StringBuilder messageBoxCS = new System.Text.StringBuilder();
+            messageBoxCS.AppendFormat("{0} = {1}", "Column", e.Column);
+            messageBoxCS.AppendLine();
+            MessageBox.Show(messageBoxCS.ToString(), "ColumnHeaderCellChanged Event");
+        }
+
+        private void dataGridMovimentacao_CellValidating(object sender, DataGridViewCellValidatingEventArgs e)
+        {
+            dataGridMovimentacao.Rows[e.RowIndex].ErrorText = "";
+            int newInteger;
+
+            // Don't try to validate the 'new row' until finished 
+            // editing since there
+            // is not any point in validating its initial value.
+            if (dataGridMovimentacao.Rows[e.RowIndex].IsNewRow) { return; }
+            if (!int.TryParse(e.FormattedValue.ToString(),
+                out newInteger) || newInteger < 0)
+            {
+                e.Cancel = true;
+                dataGridMovimentacao.Rows[e.RowIndex].ErrorText = "the value must be a non-negative integer";
+            }
+        }
+
+        private void dataGridMovimentacao_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 0)
+            {
+                if (dataGridMovimentacao.CurrentCell != null)
+                {
+                    if (int.TryParse(dataGridMovimentacao.CurrentCell.Value.ToString(), out int codigoProduto))
+                    {
+                        dataGridMovimentacao.CurrentRow.SetValues(buscaProduto(codigoProduto));
+                        //dataGridMovimentacao.CurrentCell = dataGridMovimentacao[0, 0];
+                    }
+                    else
+                        MessageBox.Show("Código inválido");
+                }
+            }
+        }
+
+        private object[] buscaProduto(int codigoProduto)
+        {
+            SqliteConnection connection;
+            String strConn = @"Data Source=" + pathSQL;
+            connection = new SqliteConnection(strConn);
+
+            string descricao = "";
+            string nomeFabricante = "";
+            string precoVenda = "";
+
+            try
+            {
+                connection.Open();
+                SqliteCommand cmd = connection.CreateCommand();
+                cmd.CommandText = $"SELECT * FROM PRODUTOS p " +
+                    $"join FABRICANTES f on f.fabricanteId = p.fabricanteId " +
+                    $"WHERE p.codigo = {codigoProduto}";
+                SqliteDataReader reader;
+                reader = cmd.ExecuteReader();
+
+
+                if (reader.Read())
+                {
+                    descricao = Convert.ToString(reader["descricao"]);
+                    nomeFabricante = Convert.ToString(reader["nome"]);
+                    precoVenda = Convert.ToString(reader["preco_venda"]);
+                }
+                reader.Dispose();
+                cmd.Dispose();
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.Message);
+            }
+            connection.Close();
+
+            return new object[] { codigoProduto, descricao, nomeFabricante, 1, precoVenda };
+        }
+
+        //private void dataGridMovimentacao_CellLeave(object sender, DataGridViewCellEventArgs e)
+        //{
+        //    //dataGridMovimentacao.CurrentRow.SetValues(new object[] { 454, "pijama", "marcyn", 4, "15,50" });
+        //    if (e.ColumnIndex == 0)
+        //    {
+        //        MessageBox.Show(dataGridMovimentacao.CurrentRow.Cells[0].FormattedValue.ToString());
+        //        if(dataGridMovimentacao.CurrentCell.Value != null)
+        //            MessageBox.Show(dataGridMovimentacao.CurrentCell.Value.ToString());
+        //        //dataGridMovimentacao.CurrentRow.SetValues(buscaProduto());
+
+
+        //        dataGridMovimentacao.CurrentRow.SetValues(new object[] { dataGridMovimentacao.CurrentCell.Value, "pijama", "marcyn", 1, "15,50" });
+        //        //dataGridMovimentacao.get
+        //    }
+        //}
     }
 }
